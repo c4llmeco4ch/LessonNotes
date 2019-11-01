9c0d08cf71d3fb6bb3bf6d43ff414cc9001006f6
c4llmeco4ch <cbb692@gmail.com>
2019-11-01 10:53:15 -0700

    removing typing for later work

--- __main__.py
+++ __main__.py
@@ -1,18 +1,18 @@
 import selenium
 from selenium import webdriver
 from datetime import date
-import typing
+'''import typing
 from typing import TypeVar
 
-DT = TypeVar('DT', date)
+DT = TypeVar('DT', date)'''
 
-def printLessons(date: datetime, displayNotes = False: bool) -> None:
+def printLessons(date, displayNotes = False):
     """
     For a given day, print out the names of the students
     If 'displayNotes' is true, also show the past lesson's note
     """
     pass
 
-def addNotes(student: str, date = today: datetime) -> bool:
+def addNotes(student, date = datetime.today):
     """Take in user inputs to send notes for a given student"""
     pass
